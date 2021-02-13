8688fd7d7172e7fcc041f3f7e67aff62f37ca48b
HongMuchan <muchang0824@gmail.com>
2021-02-13 23:10:19 +0900

    [ADD]add quest_card_components

---
+++ src/components/cards/QuestCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import styles from './QuestCard.module.scss'
+
+export default function QuestCard({ title, contact, time }) {
+  return (
+    <Card className={styles.bord}>
+      <CardContent>
+        <h3>dd{title}</h3>
+      </CardContent>
+      {contact}
+      <div className={styles.flex}>
+        <Typography className={styles.bord_time}>投稿日:{time}</Typography>
+      </div>
+    </Card>
+  )
+}
