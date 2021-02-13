0d19e32eb206375f808c5aea9d778f8c48db15c3
HongMuchan <muchang0824@gmail.com>
2021-02-14 02:10:22 +0900

    [ADD]add quest_bord_components

--- src/pages/quest_board/index.js
+++ src/pages/quest_board/index.js
@@ -1,19 +1,20 @@
 import { Layout } from '../../components/layouts/Layout/Layout'
 import QuestCard from '../../components/cards/QuestCard'
+import styles from './quest_board.module.scss'
 
 export default function quest_board({ current }) {
   const bords = [
     { id: 1, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
-    { id: 2, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
-    { id: 3, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
-    { id: 4, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
-    { id: 5, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
-    { id: 6, title: 'こんいちは', contact: 'helloworld', time: '2020-11-21' },
+    { id: 2, title: 'こんいちは', contact: 'helloworld', time: '2020-11-22' },
+    { id: 3, title: 'こんいちは', contact: 'helloworld', time: '2020-11-23' },
+    { id: 4, title: 'こんいちは', contact: 'helloworld', time: '2020-11-24' },
+    { id: 5, title: 'こんいちは', contact: 'helloworld', time: '2020-11-25' },
+    { id: 6, title: 'こんいちは', contact: 'helloworld', time: '2020-11-26' },
   ]
 
   return (
-    <div>
-      <Layout title="Guild Hack">
+    <Layout title="Guild Hack">
+      <div className={styles.bords}>
         <h1>quest_board</h1>
         <ul>
           {bords.map((bord, index) => (
@@ -22,8 +23,8 @@ export default function quest_board({ current }) {
             </li>
           ))}
         </ul>
-      </Layout>
-    </div>
+      </div>
+    </Layout>
   )
 }
 
