948ffc90b16c4f3a8446e12d6f2004647af73ebf
HongMuchan <muchang0824@gmail.com>
2021-02-14 01:03:18 +0900

    [ADD]add footer_components

---
+++ src/components/layouts/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footer_title}>@jbsfb</p>
+    </footer>
+  )
+}
