34bfdee8045ce565a844b1e649e0f1ac73e64b15
HongMuchan <muchang0824@gmail.com>
2021-02-15 16:50:32 +0900

    [CREATE]parts components

---
+++ src/components/parts/TextBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import styles from './TextBox.module.scss'
+import { data } from 'autoprefixer'
+
+export default function TextBox({ name }) {
+  return (
+    <form noValidate autoComplete="off">
+      <p className={styles.text}>{name}</p>
+      <TextField id="outlined-basic" variant="outlined" size="small" className={styles.white} />
+    </form>
+  )
+}
---
+++ src/components/parts/icons/InfraIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Icopn.module.scss'
+
+export default function InfraIcon() {
+  return (
+    <div className={styles.infra}>
+      <p className={styles.infra_text}>インフラの画像</p>
+    </div>
+  )
+}
