82fdc264a675ff8acfc29401d7c863426e5afa0a
HongMuchan <muchang0824@gmail.com>
2021-02-16 11:42:34 +0900

    [UPDATE]login index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import { Layout } from '../components/layouts/Layout/Layout'
-
+import Login from './login'
 export default function Home() {
+  const bool = true
+
   return (
     <div>
-      <Layout title="Guild Hack">
-        <h1>Home</h1>
-      </Layout>
+      {bool ? (
+        <Layout title="Guild Hack">
+          <h1>Home</h1>
+        </Layout>
+      ) : (
+        <Login />
+      )}
     </div>
   )
 }
