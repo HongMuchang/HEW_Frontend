9eb65bc37d515fd9d2178bf8594aeae4f6d4c83f
HongMuchan <muchang0824@gmail.com>
2021-02-16 11:44:39 +0900

    [CREATE]login pages

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import { Layout } from '../components/layouts/Layout/Layout'
+import LoginCard from '../components/cards/LoginCard'
+
+export default function Login() {
+  return (
+    <div>
+      <LoginCard />
+    </div>
+  )
+}
