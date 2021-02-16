97f98a646f9d0cbe8578bd704c791c8b98977360
HongMuchan <muchang0824@gmail.com>
2021-02-16 23:00:54 +0900

    [UPDATE]not update checkbox

--- src/components/parts/CheckBox.jsx
+++ src/components/parts/CheckBox.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField'
+import React, { useState } from 'react'
 import styles from './CheckBox.module.scss'
-import FormControl from '@material-ui/core/FormControl'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FrontIcon from './icons/FrontIcon'
-import BackIcon from './icons/BackIcon'
-import InfraIcon from './icons/InfraIcon'
+import { Radio, RadioGroup, FormControl, FormControlLabel, TextField } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+import { createdbord } from '../../features/bord/bordSlice'
 
-export default function TextBox({ title, one, two, three }) {
+export default function TextBox({ title, one, two, three, setCheck, name }) {
+  //何が押されているか
+  const [a, setA] = useState('')
+
+  const handleChange = (e) => setA(e)
+
+  const bords = useSelector(createdbord)
+  setCheck(a)
+
+  // const dispatch = useDispatch()
+
+  //最終のaの値を親に
+
+  // dispatch(createdbord({ name: a }))
+
   return (
     <div className={styles.checkBox}>
       <p>{title}</p>
       <FormControl component="fieldset">
         <RadioGroup aria-label="gender" name="position">
           <div className={styles.flex}>
-            <FormControlLabel value={one} control={<Radio />} label={one} />
-            <FormControlLabel value={two} control={<Radio />} label={two} />
-            <FormControlLabel value={three} control={<Radio />} label={three} />
+            <FormControlLabel
+              value={one}
+              control={<Radio />}
+              label={one}
+              onChange={(e) => handleChange(e.target.value)}
+              checked={name === { one }}
+            />
+            <FormControlLabel
+              value={two}
+              control={<Radio />}
+              label={two}
+              onChange={(e) => handleChange(e)}
+              checked={name === { two }}
+            />
+            <FormControlLabel
+              value={three}
+              control={<Radio />}
+              label={three}
+              onChange={(e) => handleChange(e)}
+              checked={name === { three }}
+            />
           </div>
         </RadioGroup>
       </FormControl>
