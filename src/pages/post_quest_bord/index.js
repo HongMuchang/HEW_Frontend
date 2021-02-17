1ea51cff01f07d008ba346e72568ee8fc570997b
HongMuchan <muchang0824@gmail.com>
2021-02-18 01:00:06 +0900

    [ADD]add entry_point

--- src/pages/post_quest_bord/index.js
+++ src/pages/post_quest_bord/index.js
@@ -1,16 +1,10 @@
 import { Layout } from '../../components/layouts/Layout/Layout'
-import styles from './post_quest_bord.module.scss'
-import PostQuestCard from '../../components/cards/PostQuestCard'
+import { Bord } from '../../features/bord/index'
 
 export default function post_quest_bord() {
   return (
     <Layout title="Guild Hack">
-      <div className={styles.bords}>
-        <h1>post_quest_board</h1>
-        <div className={styles.bord}>
-          <PostQuestCard />
-        </div>
-      </div>
+      <Bord />
     </Layout>
   )
 }
