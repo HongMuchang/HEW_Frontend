c2caea67698bbec3035984eaafdb101562a7769e
HongMuchan <muchang0824@gmail.com>
2021-02-22 15:20:06 +0900

    [UPDATE]#44 update layout

--- src/pages/quest_bord/index.js
+++ src/pages/quest_bord/index.js
@@ -5,14 +5,15 @@ import { getAllPostsData } from '../../../lib/post'
 import Link from 'next/link'
 
 export default function quest_bord({ posts }) {
+  console.log(posts)
   return (
     <Layout title="Guild Hack">
       <div className={styles.bords}>
         <h1>quest_board</h1>
         <ul>
-          {posts.map((post, index) => (
+          {posts.map((post) => (
             <Link href={`/quest_bord/${post.id}`}>
-              <li key={index}>
+              <li key={post}>
                 <QuestCard title={post.title} contact={post.body} time={post.id} />
               </li>
             </Link>
