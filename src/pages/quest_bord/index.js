8fcb4508f70df23f07d11d46d43a507c8877fd30
HongMuchan <muchang0824@gmail.com>
2021-03-02 12:44:30 +0900

    [UPDATE]#54 delete comemts

--- src/pages/quest_bord/index.js
+++ src/pages/quest_bord/index.js
@@ -3,8 +3,14 @@ import QuestCard from '../../components/cards/QuestCard'
 import styles from './quest_bord.module.scss'
 import { getAllPostsData } from '../../../lib/post'
 import Link from 'next/link'
+import Login from '../../pages/login'
+import { auth } from '../../utils/firebase'
 
 export default function quest_bord({ posts }) {
+  // if (auth.currentUser == null) {
+  //   return <Login />
+  // }
+
   console.log(posts)
   return (
     <Layout title="クエスト一覧画面">
@@ -14,6 +20,7 @@ export default function quest_bord({ posts }) {
           {posts.map((post) => (
             <Link href={`/quest_bord/${post.id}`}>
               <li key={post}>
+                {post.id}
                 <QuestCard title={post.title} contact={post.body} time={post.updated} />
               </li>
             </Link>
