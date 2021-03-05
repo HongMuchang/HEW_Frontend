a541909813a4ec51001017b39f94e79de3e0a9c7
HongMuchan <muchang0824@gmail.com>
2021-03-05 09:48:42 +0900

    [UPDATE]#61 update postiong components

--- src/pages/posting_bord/index.js
+++ src/pages/posting_bord/index.js
@@ -1,27 +1,27 @@
 import { Layout } from '../../components/layouts/Layout/Layout'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 
 export default function posting_bord() {
   const user_masterid = useSelector((state) => state.user) //今まで追加された数
+  const uid = user_masterid.user.uid // uid
+  const [date, setDate] = useState()
 
-  console.log(user_masterid)
+  console.log(uid)
 
-  const go = () => {
+  useEffect(() => {
     axios
-      .get('http://localhost:60001/users/in-posts', { headers: { uid: 'uejjoencoajeicms' } })
-      .then(function (response) {
-        console.log(response)
-      })
+      .get('http://localhost:60001/users/in-posts', { headers: { uid: uid } })
+      .then((response) => setDate(response.data))
       .catch(function (error) {
         console.log(error)
       })
-  }
+  }, [])
 
   return (
     <Layout title="クエスト募集画面">
       <p>posting</p>
-      <button onClick={() => go()}>ddd</button>
     </Layout>
   )
 }
