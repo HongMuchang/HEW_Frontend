183dbc23f8fb7f217264c61afc5e1783c151e645
HongMuchan <muchang0824@gmail.com>
2021-03-06 13:32:40 +0900

    [UPDATE]#63 axios path

--- src/pages/posting_bord/index.js
+++ src/pages/posting_bord/index.js
@@ -2,6 +2,7 @@ import { Layout } from '../../components/layouts/Layout/Layout'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
 import { useState, useEffect } from 'react'
+import { green } from '@material-ui/core/colors'
 
 export default function posting_bord() {
   const user_masterid = useSelector((state) => state.user) //今まで追加された数
@@ -9,10 +10,11 @@ export default function posting_bord() {
   const [date, setDate] = useState()
 
   console.log(uid)
+  console.log(date)
 
   useEffect(() => {
     axios
-      .get('http://localhost:60001/users/in-posts', { headers: { uid: uid } })
+      .get('http://localhost:60001/users/in-posts', { headers: { uid: 'kIaKVIiatEcCibka' } })
       .then((response) => setDate(response.data))
       .catch(function (error) {
         console.log(error)
